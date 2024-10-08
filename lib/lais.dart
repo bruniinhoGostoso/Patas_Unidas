64b318a6923528b8223219f74ad923ba026631b2
jeniferlais <jlsn3@aluno.ifal.edu.br>
2024-10-08 10:25:49 -0300

    branch lais

---
+++ lib/lais.dart
@@ -0,0 +1,13 @@
+class lais extends StatefulWidget {
+  const lais({super.key});
+
+  @override
+  State<lais> createState() => _laisState();
+}
+
+class _laisState extends State<lais> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
