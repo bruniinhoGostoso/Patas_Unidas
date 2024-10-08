d528f758226e66de1f5c5c9c218a74202211078a
jeniferlais <jlsn3@aluno.ifal.edu.br>
2024-10-08 10:12:21 -0300

    segunda tentatva

---
+++ android/app/src/main/kotlin/com/example/patas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.patas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/testando_oi.dart
@@ -0,0 +1,13 @@
+class testando_oi extends StatefulWidget {
+  const testando_oi({super.key});
+
+  @override
+  State<testando_oi> createState() => _testando_oiState();
+}
+
+class _testando_oiState extends State<testando_oi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
