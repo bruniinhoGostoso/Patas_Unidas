386671139da5d62d1b826e136678904be4523f54
jeniferlais <jlsn3@aluno.ifal.edu.br>
2024-10-08 10:14:31 -0300

    segunda tentatva branch

---
+++ lib/oioi.dart
@@ -0,0 +1,13 @@
+class oioi extends StatefulWidget {
+  const oioi({super.key});
+
+  @override
+  State<oioi> createState() => _oioiState();
+}
+
+class _oioiState extends State<oioi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
